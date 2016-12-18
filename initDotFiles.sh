1555ca997dd4edd94747c114565998eaa8091881
tmrtkn <toni.martikainen@gmail.com>
2016-12-19 00:13:21 +0200

    Added a script that links the dotfiles to user home dir

---
+++ initDotFiles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SCRIPT=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT")
+
+FILES=(.bashrc .vimrc .gitconfig)
+
+for file in ${FILES[@]}; do
+    if [ -f $file ]; then
+    	mv ~/$file ~/$file.original
+    fi
+    ln -s $SCRIPT_DIR/$file ~/$file
+done
+
+exit 0
