e8f0a50a8148749da15d9e164d0dffd361b8adc6
martikat <toni.martikainen@solita.fi>
2019-01-28 08:56:41 +0200

    Added files for possible i3wm configuration

--- initDotFiles.sh
+++ initDotFiles.sh
@@ -3,7 +3,7 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT")
 
-FILES=(.bashrc .vimrc .gitconfig .tmux.conf)
+FILES=(.bashrc .vimrc .gitconfig .tmux.conf .xinitrc .Xresources)
 
 for file in ${FILES[@]}; do
     if [ -f $file ]; then
@@ -11,5 +11,24 @@ for file in ${FILES[@]}; do
     fi
     ln -s $SCRIPT_DIR/$file ~/$file
 done
+
+
+if [ -d ~/.config/i3 ]; then
+  mv ~/.config/i3 ~/.config/i3.original
+fi
+mkdir -p ~/.config/
+ln -s $SCRIPT_DIR/.config/i3 ~/.config/i3
+
+if [ -d ~/.config/i3status ]; then
+  mv ~/.config/i3status ~/.config/i3status.original
+fi
+mkdir -p ~/.config/
+ln -s $SCRIPT_DIR/.config/i3status ~/.config/i3status
+
+if [ -d ~/.config/fontconfig ]; then
+  mv ~/.config/fontconfig ~/.config/fontconfig.original
+fi
+mkdir -p ~/.config/
+ln -s $SCRIPT_DIR/.config/fontconfig ~/.config/fontconfig
 
 exit 0
