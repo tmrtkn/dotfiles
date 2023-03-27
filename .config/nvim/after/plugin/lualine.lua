22932ba095382526f20b669e0c604f4ff9165e2d
Toni Martikainen <toni.martikainen@tietoevry.com>
2023-03-27 14:33:34 +0300

    Use relative filepath in the file names

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -178,7 +178,8 @@ ins_left {
 
 ins_left {
   'filename',
-  cond = conditions.buffer_not_empty,
+  path = 1, -- show relational file path with the filename
+  kcond = conditions.buffer_not_empty,
   color = { fg = colors.magenta, gui = 'bold' },
 }
 
